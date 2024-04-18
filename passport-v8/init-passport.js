934a0a2206cc5e14a8271f1561734524f75047a7
Mike Avvad <mavvad00@gmail.com>
2024-04-18 11:17:48 +0200

    init passport change script

--- passport-v8/init-passport.js
+++ passport-v8/init-passport.js
@@ -1,23 +1,25 @@
-const CHAIND_ID = '80001';
-
 const $clientIdContainer = document.querySelector(`#credenza-client-id`);
 const clientId = $clientIdContainer?.getAttribute("data-credenza-client-id");
 
-window.passport = new window.CredenzaPassport({
-  env: "staging",
-  chainId: CHAIND_ID,
-  clientId,
-  config: {
-    auth: {
-      metamask: false,
-      google: false,
-      ticketmaster: false,
-      passwordless: false
-    }
-  },
-});
+const DEFAULT_CHAIN_ID = '137';
 
-export const initPassport = async () => {
+export const initPassport = async (chainId = DEFAULT_CHAIN_ID) => {
+  if (!chainId.trim()) chainId = DEFAULT_CHAIN_ID;
+  console.log('Passport is initiated on chain id: ', chainId)
+  window.passport = new window.CredenzaPassport({
+    env: "staging",
+    chainId,
+    clientId,
+    config: {
+      auth: {
+        metamask: false,
+        google: false,
+        ticketmaster: false,
+        passwordless: false
+      }
+    },
+  });
+
   await passport.init();
   return passport;
 };
