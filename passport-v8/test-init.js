91264867e97cc01fb62b6819fef535dfae4d8893
Mike Avvad <mavvad00@gmail.com>
2024-04-18 11:13:41 +0200

    fix default chain id

--- passport-v8/test-init.js
+++ passport-v8/test-init.js
@@ -1,9 +1,10 @@
 const $clientIdContainer = document.querySelector(`#credenza-client-id`);
 const clientId = $clientIdContainer?.getAttribute("data-credenza-client-id");
 
+const DEFAULT_CHAIN_ID = '137';
 
-
-export const initPassport = async (chainId = '137') => {
+export const initPassport = async (chainId = DEFAULT_CHAIN_ID) => {
+  if (!chainId.trim()) chainId = DEFAULT_CHAIN_ID;
   console.log('Passport is initiated on chain id: ', chainId)
   window.passport = new window.CredenzaPassport({
     env: "staging",
