1bac9b036e641a3451cddd3b09a46acb8c408b76
Mike Avvad <mavvad00@gmail.com>
2024-04-18 11:06:58 +0200

    added test file to text new init

---
+++ passport-v8/test-init.js
@@ -0,0 +1,23 @@
+const $clientIdContainer = document.querySelector(`#credenza-client-id`);
+const clientId = $clientIdContainer?.getAttribute("data-credenza-client-id");
+
+
+
+export const initPassport = async (chainId = '137') => {
+  window.passport = new window.CredenzaPassport({
+    env: "staging",
+    chainId,
+    clientId,
+    config: {
+      auth: {
+        metamask: false,
+        google: false,
+        ticketmaster: false,
+        passwordless: false
+      }
+    },
+  });
+
+  await passport.init();
+  return passport;
+};
